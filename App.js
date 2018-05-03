c0f3342084a6d030683a6697d509e7f14ed0a270
Daniel Sanchez <jose.sanchez@ironbit.com.mx>
2018-05-02 19:25:28 -0500

    testing react-navigation

--- App.js
+++ App.js
@@ -1,31 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Bananas from "./components/Bananas";
-import FlatListBasics from "./components/FlatListBasics";
-import SectionListBasics from "./components/SectionListBasics";
-import FetchExample from "./components/FetchExample";
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+import { StackNavigator } from "react-navigation";
 
-export default class App extends React.Component {
+class DetailsScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Bananas />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <FlatListBasics/>
-        <SectionListBasics/>
-        <FetchExample/>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate("Details")}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen
+    },
+    Details: {
+      screen: DetailsScreen
+    }
   },
-});
+  {
+    initialRouteName: "Home"
+  }
+);
+
+export default class App extends Component {
+  render() {
+    return <RootStack />;
+  }
+}
